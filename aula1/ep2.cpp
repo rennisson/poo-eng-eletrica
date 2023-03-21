1e62a4f780ae4efc55de1243c3dd05ed18bdf7b0
unknown <Rennisson Davi Domingos Alves>
2023-03-21 15:19:45 -0300

    first commit - aula 1

---
+++ aula1/ep2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int tempoVisualizacaoTotal(int visualizacoes[], int duracoes[], int quantidade);
+
+int main() {
+    std::cout << "------ TESTES DA CLASSE tempoVisualizacaoTotal() -------\n";
+
+    int visualizacoes1[] = { 16, 21, 12 };
+    int duracoes1[] = { 10, 20, 30 };
+    int quantidade = sizeof(visualizacoes1) / sizeof(visualizacoes1[0]);
+
+    std::cout << "tempoVisualizacaoTotal(visualizacoes, duracoes, quantidade) retornou "
+                << tempoVisualizacaoTotal(visualizacoes1, duracoes1, quantidade)
+                << " e deve retornar 940\n";
+
+    int visualizacoes2[] = { 10, 10, 10, 10, 10, 10 };
+    int duracoes2[] = { 10, 10, 10, 10, 10, 10 };
+    quantidade = sizeof(visualizacoes2) / sizeof(visualizacoes2[0]);
+
+    std::cout << "tempoVisualizacaoTotal(visualizacoes, duracoes, quantidade) retornou "
+                << tempoVisualizacaoTotal(visualizacoes2, duracoes2, quantidade)
+                << " e deve retornar 600\n";
+
+    int visualizacoes3[] = { 10 };
+    int duracoes3[] = { 3 };
+    quantidade = sizeof(visualizacoes3) / sizeof(visualizacoes3[0]);
+
+    std::cout << "tempoVisualizacaoTotal(visualizacoes, duracoes, quantidade) retornou "
+                << tempoVisualizacaoTotal(visualizacoes3, duracoes3, quantidade)
+                << " e deve retornar 30\n";
+}
+
+int tempoVisualizacaoTotal(int visualizacoes[], int duracoes[], int quantidade) {
+
+    int tempoTotal = 0;  // guarda o tempo total de visualizacoes
+
+    for (int i = 0; i < quantidade; i++) {
+        tempoTotal += visualizacoes[i] * duracoes[i];
+    }
+
+    return tempoTotal;
+}
---
+++ aula1/ep3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+bool assistiuVideosRepetidos(std::string urls[], int quantidade);
+
+int main() {
+    std::cout << "------ TESTES DA CLASSE assistiuVideosRepetidos() -------\n";
+
+    std::string urls1[] = {"zI_cLtW3PnA", "uvh8wVgLTS8", "zI_cLtW3PnA"}; 
+    int quantidade = sizeof(urls1) / sizeof(urls1[0]);
+    std::cout << "assistiuVideosRepetidos({'zI_cLtW3PnA', 'uvh8wVgLTS8', 'zI_cLtW3PnA'}, 3) retornou "
+                << assistiuVideosRepetidos(urls1, 3)
+                << " e deve retornar TRUE\n";
+
+    std::string urls2[] = {"zI_cLtW3PnA", "uvh8wVgLTS8", "g7C2it9cCsY", "uvh8wVgLTS8", "g7C2it9cCsY"}; 
+    quantidade = sizeof(urls2) / sizeof(urls2[0]);
+    std::cout << "assistiuVideosRepetidos({'zI_cLtW3PnA', 'uvh8wVgLTS8', 'g7C2it9cCsY', 'uvh8wVgLTS8', 'g7C2it9cCsY'}, 3) retornou "
+                << assistiuVideosRepetidos(urls2, 5)
+                << " e deve retornar TRUE\n";
+}
+
+bool assistiuVideosRepetidos(std::string urls[], int quantidade) {
+    // Não é necessário comparar o ultimo elemento do array, por isso utilizamos (quantidade - 1)
+    for (int i = 0; i < quantidade - 1; i++) {
+        for (int j = 0; j < quantidade - i - 1; j++) {
+            if (urls[i] == urls[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
