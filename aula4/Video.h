8d665d54eb17e3bf0b829fd6e155b0407d441b0f
rennisson <rennisson.alves@gmail.com>
2023-04-16 20:17:16 -0300

    Header da classe Video

---
+++ aula4/Video.h
@@ -0,0 +1,23 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <string>;
+using namespace std;
+
+class Video {
+private: 
+  string nome;
+  int duracao;
+  int visualizacoes = 0;
+
+public:
+  string getNome();
+  void setNome(string nome);
+  int getDuracao();
+  void setDuracao(int duracao);
+  int getVisualizacoes();
+  void assistir (int tempo);    
+  void imprimir ();
+};
+
+#endif // VIDEO_H
