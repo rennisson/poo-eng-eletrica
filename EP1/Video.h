344384089f0dbc8ceccec84c0ce86906c911c297
rennisson <rennisson.alves@gmail.com>
2023-05-28 21:06:08 -0300

    docs Video

--- EP1/Video.h
+++ EP1/Video.h
@@ -5,12 +5,15 @@
 
 using namespace std;
 
+/**
+ *  É um tipo de Conteudo que pode ser assistido e adicionado em Canal. É uma subclasse de Conteudo.
+*/
 class Video : public Conteudo {
-public:
-  Video(string nome, int duracao);
-  virtual ~Video();
-  virtual void assistir(int tempo);    
-  virtual void imprimir();
+  public:
+    Video(string nome, int duracao);
+    virtual ~Video();
+    virtual void assistir(int tempo);    
+    virtual void imprimir();
 };
 
 #endif // VIDEO_H
